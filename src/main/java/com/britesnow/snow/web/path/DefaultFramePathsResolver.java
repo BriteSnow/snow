131fd59c9397714cc498916ebf2c07933fa68e4e
jeremychone <jeremy.chone@gmail.com>
2015-05-26 10:00:41 -0700

    + added DefaultFramePathsResolver.resolveResourcePath(String path, RequestContext rc) for convenience (allow to reuse the framePaths resolution for custom path)

--- src/main/java/com/britesnow/snow/web/path/DefaultFramePathsResolver.java
+++ src/main/java/com/britesnow/snow/web/path/DefaultFramePathsResolver.java
@@ -16,14 +16,27 @@ public class DefaultFramePathsResolver implements FramePathsResolver {
     @Inject
     private ResourceFileLocator      pathFileResolver;
 
+    /**
+     * By default, return the framPath for the resourcePath.
+     */
     @Override
     public String[] resolve(RequestContext rc) {
-
         String path = rc.getResourcePath();
+        return resolveResourcePath(path, rc);
+    }
+
+    /**
+     * <p>Build the FramePath array from a path (e.g., "/blog/html5/my-html5-architecture").</p>
+     * <p>Note that RequestContext hee is just for the pathFileResolve that requires it (not used for the path, as it is given as parameter)</p>
+     *
+     * @param path
+     * @return
+     */
+    public String[] resolveResourcePath(String path, RequestContext rc){
         String framePath;
-        
+
         List<String> framePaths = new ArrayList<String>();
-        
+
         List<String> pathArray = new ArrayList<String>();
         Iterables.addAll(pathArray, Splitter.on("/").split(path));
         for (int i = pathArray.size() -1; i > 0; i--){
@@ -32,15 +45,15 @@ public class DefaultFramePathsResolver implements FramePathsResolver {
             if (!framePath.startsWith("/")){
                 framePath = "/" + framePath;
             }
-            
+
             // TODO: need to change this harcoded ".ftl" when we support multi template types
             if (pathFileResolver.locate(framePath + ".ftl",rc).exists()){
                 framePaths.add(framePath);
             }
         }
-        
+
         framePaths = Lists.reverse(framePaths);
-        
+
         return framePaths.toArray(new String[framePaths.size()]);
     }
 
